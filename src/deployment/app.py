448ed8f21a81b2c457d4c807b44edc86e6c66895
Nick Todts <nick-todts@hotmail.com>
2019-12-29 21:18:34 +0100

    Send unicode of predicted letter to the view.

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_bootstrap import Bootstrap
-from fastai import *
 from fastai.vision import *
 
 
@@ -22,6 +21,19 @@ def predict(p, img_name):
     return predicted_class
 
 
+def get_hebrew_unicode(letter):
+    hebrew_unicodes = {
+        'alef': '&#1488;', 'bet': '&#64305;', 'vet': '&#1489;', 'gimel': '&#1490; ', 'dalet': '&#1491;',
+        'he': '&#1492;', 'vav': '&#1493;', 'zayin': '&#1494;', 'chet': '&#1495;', 'tet': '&#1496;',
+        'yod': '&#1497;', 'kaf': '&#64315;', 'kaf-final': '&#64314;', 'chaf': '&#1499; ', 'chaf-final': '&#1498;',
+        'lamed': '&#1500;', 'mem': '&#1502; ', 'mem-final': '&#1501;', 'nun': '&#1504;', 'nun-final': '&#1503;',
+        'samech': '&#1505;', 'ayin': '&#1506;', 'pei': '&#64324; ', 'pei-final': ' 	&#64323;', 'fei': '&#1508;',
+        'fei-final': '&#1507;', 'tsade': '&#1510;', 'tsade-final': '&#1509;', 'quf': '&#1511;', 'reish': '&#1512; ',
+        'shin': '&#64298;', 'sin': '&#64299;', 'tav': '&#1514;'
+    }
+    return hebrew_unicodes.get(letter).strip()  # todo insert default value if it can't find anything
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
@@ -31,7 +43,6 @@ def index():
 @app.route("/classify", methods=['POST'])
 def classify():
     if 'file' in request.files:
-        print('hooray')
         img_file = request.files.get('file')
         img_name = img_file.filename
 
@@ -41,15 +52,19 @@ def classify():
 
         # Make a prediction.
         prediction = predict(p, img_name)
-        print("prediction: ", prediction)
+        prediction, _ = str(prediction).split('-')  # e.g. alef-1 --> alef
+
+        hebrew_char = get_hebrew_unicode(prediction)
 
         # Delete image from tmp folder.
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], img_name))
 
     else:
         print("error")
+        # todo render error page?
 
-    return redirect(url_for('index'))
+#    return redirect(url_for('index'))
+    return render_template("index.html", prediction=prediction, hebrew_char=hebrew_char)
 
 
 if __name__ == '__main__':
